e61cebf59296fcec3fce09a73a7f2760112e4d1e
rgmotisariya <bhobhoomi8@gmail.com>
2024-06-14 14:33:20 +0530

    chanded

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
-  build: {
-    rollupOptions: {
-      external: ['react-icons/rx']
-    }
-  }
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Frontend/",
+    build: {
+      rollupOptions: {
+        external: ['react-icons/rx']
+      }}
+
 })
+ 
